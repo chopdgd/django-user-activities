2c4a54d3d61b6329e7748a47e30a1e0f074fed16
genomics-geek <GonzalezMA@email.chop.edu>
2019-05-31 10:29:50 -0400

    fixed urls

--- tests/urls.py
+++ tests/urls.py
@@ -4,11 +4,9 @@ from __future__ import unicode_literals, absolute_import
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from user_activities.urls import urlpatterns as user_activities_urls
-
 
 app_name = 'user_activities'
 urlpatterns = [
-    url(r'^', include(user_activities_urls)),
+    url(r'^', include('user_activities.urls')),
     url(r'^admin/', admin.site.urls),
 ]
